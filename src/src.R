f5df068091f51cec28dade386fdf43ee5c7142de
Bartek Ciulkin <bartosz.ciulkin@gmail.com>
2018-04-01 12:44:23 +0200

    Counts random vector components (R1 to R6) based on tStudent distribution

--- src/src.R
+++ src/src.R
@@ -1,9 +1,9 @@
 library(mvtnorm)
 
-#parametry wejsciowe
-przedzial = data.frame(bottom = 80, top = 120)
+#input params
+dataRange = data.frame(bottom = 80, top = 120)
 mu = c(116, 102, 113, 100, 107, 110)
-degresOfFreedom = 5
+degreesOfFreedom = 5
 data = c(1, 1, 0, 1, 1, 1,
          1,36,-1,-1,-3,-5,
          0,-1, 4, 2, 2, 0,
@@ -12,18 +12,48 @@ data = c(1, 1, 0, 1, 1, 1,
          1,-5, 0,-2,-2, 9)
 sigma = matrix(data = data, nrow = 6, ncol = 6)
 
-countExpected = function(mu, sig, df, prze = przedzial) {
+
+main = function () {
+  randomVectorComponents = c()
+  randomVectorComponents = countRandomVectorComponents()
+  
+  for (j in 1:6){
+    print(randomVectorComponents[j])
+  }
+  
+  #ddd = data.frame(A = c(R1, R4), B = c(R2, R5), C = c(R3, R6))
+  
+  # R5
+  #ddd[2, 2]; ddd$B[2]; ddd[[2]][2]; ddd[names(ddd) == "B", 2]
+  
+  # R6
+  #ddd[2, 3]; ddd$C[2]; ddd[[3]][2]
+  
+}
+
+countExpectedValue = function (mu, sig, df = degreesOfFreedom, dr = dataRange) {
   if (df > 1) {
-    a = (prze$bottom - mu)/sig
-    b = (prze$top    - mu)/sig
+    a = (dr$bottom - mu)/sig
+    b = (dr$top    - mu)/sig
     
-    licznik = gamma( ((df-1)/2))*((df + a^2)^(-1*(df-1)/2) -
+    nominator = gamma( ((df-1)/2))*((df + a^2)^(-1*(df-1)/2) -
                        (df + b^2)^(-1*(df-1)/2))  * df^(df/2)
     
-    mianownik = 2*(pt(b, df) - pt(a, df)) * gamma(df/2) * gamma(1/2)
+    denominator = 2*(pt(b, df) - pt(a, df)) * gamma(df/2) * gamma(1/2)
     
-    return(mu + sig*licznik/mianownik)
-  } else {
-    stop("countExpected: Degrees of freedom should be > 1")
-  }
+    return (mu + sig * nominator / denominator)
+    
+  } else { stop("countExpected: Degrees of freedom should be > 1") }
 }
+
+countRandomVectorComponents = function () {
+  randomVectorElements = c()
+  for (i in 1:6) {
+    randomVectorElements[i] = countExpectedValue(mu[i], sigma[i, i])
+  }
+  
+  return (randomVectorElements)
+}
+
+main()
+
--- tests/tests.R
+++ tests/tests.R
@@ -2,6 +2,6 @@ library(testthat)
 source("src/src.R")
 
 test_that("Function countExpected counts correct number based on tStudent.pdf", {
-  expect_equal(round(countExpected(45, 1, 4, data.frame(bottom = 20, top = 50)), 2), 44.97)
+  expect_equal(round(countExpectedValue(45, 1, 4, data.frame(bottom = 20, top = 50)), 2), 44.97)
 })
 
