108f26feb873c07fe8e02dd1053b0d3324943c02
Bartek Ciulkin <bartosz.ciulkin@gmail.com>
2018-05-16 22:23:44 +0200

    Wersja druga scenariuszy

--- src/R/scenariusze.r
+++ src/R/scenariusze.r
@@ -14,7 +14,6 @@ data = c(1, 1, 0, 1, 1, 1,
 sigma = matrix(data = data, nrow = 6, ncol = 6)
 
 all_cases <- rmvt(10000, sigma, degreesOfFreedom, mu)
-#all_cases <- rmvt(10000, mu,sigma, tol, empirical, eispack)
 all_cases <- all_cases[all_cases[,1]>=80,]
 all_cases <- all_cases[all_cases[,2]>=80,]
 all_cases <- all_cases[all_cases[,3]>=80,]
@@ -29,6 +28,6 @@ all_cases <- all_cases[all_cases[,5]<=120,]
 all_cases <- all_cases[all_cases[,6]<=120,]
 all_cases <- all_cases[1:1000,]
 
-write.table(all_cases, "scenariusze.data", sep=" ", eol="]\n[", row.names = FALSE, col.names=FALSE)
+write.table(all_cases, "scenariusze2.data", sep=" ", eol="\n", row.names = FALSE, col.names=FALSE)
 
 
